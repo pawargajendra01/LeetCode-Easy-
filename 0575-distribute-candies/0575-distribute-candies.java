2648f1f8d7caa5d44c581b3d765bf412a6e24a43
Gajendra Pawar <gajendra101004@gmail.com>
2026-02-21 00:42:43 +0530

    Time: 37 ms (21.82%), Space: 48.9 MB (15.68%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Arrays.sort(candyType);
+        int count=1;
+        for(int i=0;i<candyType.length-1;i++){
+            if(candyType[i]<candyType[i+1]){
+                count++;
+            }
+        }
+        if(count>candyType.length/2){
+            return candyType.length/2;
+        }
+        else{
+            return count;
+        }
+    }
+}
