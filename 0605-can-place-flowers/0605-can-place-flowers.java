24f6340d101725f3c5fe9c5763f616b490b1daef
Gajendra Pawar <gajendra101004@gmail.com>
2026-02-23 23:14:33 +0530

    Time: 1 ms (98.57%), Space: 48.1 MB (16.95%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,45 +1,16 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int count = 0;
-        int result = 0;
-        boolean firstone = true;
-        if(flowerbed.length==1 && flowerbed[0]==0 && n<2){
-            return true;
-        }
-        for (int i = 0; i < flowerbed.length; i++) {
-            if (flowerbed[i] == 0) {
-                count++;
-            } else if (firstone) {
-                if (count > 1) {
-                    result = result + (count / 2);
+        for (int i = 0; i < flowerbed.length && n > 0; i++) {
+            if (flowerbed[i] == 0) {       
+                int prev = (i == 0) ? 0 : flowerbed[i - 1];
+                int next = (i == flowerbed.length - 1) ? 0 : flowerbed[i + 1];
+                
+                if (prev == 0 && next == 0) {
+                    flowerbed[i] = 1;
+                    n--;
                 }
-                count = 0;
-                firstone=false;
-            } else {
-                if (count > 2) {
-                    if (count % 2 == 1) {
-                        result = result + (count / 2);
-                    } else {
-                        result = result + ((count / 2) - 1);
-                    }
-                }
-                count = 0;
-            }
-            if(i==flowerbed.length-1 && firstone){
-                if (count % 2 == 1) {
-                        result = result + (count / 2)+1;
-                    } else {
-                        result = result + (count / 2);
-                    }
-                return result>=n;
-            }
-            if(i==flowerbed.length-1&&count>1){
-                result = result + (count / 2);
             }
         }
-        if (result >= n) {
-            return true;
-        } else
-            return false;
+        return n == 0;
     }
 }
