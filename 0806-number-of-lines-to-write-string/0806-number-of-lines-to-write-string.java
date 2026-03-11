79625f0a71cafdf4ab3cf7a8cfcb371d3b305029
Gajendra Pawar <gajendra101004@gmail.com>
2026-03-12 00:16:27 +0530

    Time: 1 ms (43.52%), Space: 43 MB (89.67%) - LeetHub

--- 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.java
+++ 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.java
@@ -1,18 +1,22 @@
 class Solution {
     public int[] numberOfLines(int[] widths, String s) {
-        int lines = 1;
-        int currentLineWidth = 0;
 
-        for (char c : s.toCharArray()) {
-            int w = widths[c - 'a'];
-            if (currentLineWidth + w > 100) {
-                lines++;
-                currentLineWidth = w;
-            } else {
-                currentLineWidth += w;
+        int[] arr = new int[2];
+        int total=0;
+        int count=1;
+
+        for(int i=0;i<s.length();i++){
+            total+= widths[s.charAt(i) - 'a'];
+            if(total>100){
+                total-=widths[s.charAt(i) - 'a'];
+                total=0;
+                total+= widths[s.charAt(i) - 'a'];
+                count++;
             }
         }
+        arr[0]=count;
+        arr[1]=total;
 
-        return new int[]{lines, currentLineWidth};
+        return arr;
     }
 }
