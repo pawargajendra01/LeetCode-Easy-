4072c9bacc9f12ab1fdae4520edff9b720378bf6
Gajendra Pawar <gajendra101004@gmail.com>
2026-03-22 16:45:24 +0530

    Time: 3 ms (42.12%), Space: 47.4 MB (38.3%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int[] arr = new int[s.length()+1];
+        int left=0;
+        int right=s.length();
+        for(int i=0;i<s.length();i++){
+                if(s.charAt(i)=='I'){
+                    arr[i]=left;
+                    left++;
+                }else{
+                    arr[i]=right;
+                    right--;
+                }
+        }
+        arr[s.length()]=left;
+        return arr;
+    }
+}
