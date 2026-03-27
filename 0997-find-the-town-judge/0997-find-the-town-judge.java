b8a5747f820138f51d123ff7ebcf9507d25ea3fe
Gajendra Pawar <gajendra101004@gmail.com>
2026-03-27 23:02:53 +0530

    Time: 2 ms (99.83%), Space: 53.4 MB (87.86%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,17 +1,22 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int[] scores = new int[n + 1];
-        
-        for (int[] t : trust) {
-            scores[t[0]]--; // Trusts someone? Lose a point.
-            scores[t[1]]++; // Is trusted? Gain a point.
+        if(trust.length==0 && n==1){
+            return 1;
         }
-        
-        for (int i = 1; i <= n; i++) {
-            // Only the judge can have exactly n-1 points
-            if (scores[i] == n - 1) return i;
+
+        int indegree[] = new int[n+1];
+        int outdegree[] = new int[n+1];
+
+        for(int[] t:trust){
+            outdegree[t[0]]++;
+            indegree[t[1]]++;
         }
-        
+
+        for(int i=0;i<n+1;i++){
+            if(indegree[i]==n-1 && outdegree[i]==0){
+                return i;
+            }
+        }
         return -1;
     }
 }
