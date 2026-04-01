6459c3e9b4919b6d57d4a7cc603f4fb13cbd2164
Gajendra Pawar <gajendra101004@gmail.com>
2026-04-01 23:21:58 +0530

    Time: 6 ms (88.74%), Space: 44.8 MB (25.29%) - LeetHub

---
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length && k > 0; i++) {
+            if (nums[i] < 0) {
+                nums[i] = -nums[i];
+                k--;
+            }
+        }
+        if (k % 2 != 0) {
+            Arrays.sort(nums);
+            nums[0] = -nums[0];
+        }
+        int sum = 0;
+        for (int n : nums)
+            sum += n;
+        return sum;
+    }
+}
