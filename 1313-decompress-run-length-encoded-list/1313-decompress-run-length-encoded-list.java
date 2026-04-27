b5830567db7572258a095f31eec3b50562ab06f7
Gajendra Pawar <gajendra101004@gmail.com>
2026-04-27 20:38:34 +0530

    Time: 0 ms (100%), Space: 46.7 MB (18.51%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
@@ -9,26 +9,9 @@ class Solution {
         for (int i = 0; i < nums.length; i+=2) {
             int freq = nums[i];
             int val = nums[i+1];
-            for (int j = 1; j <= freq; j++){
-                arr[num]=val;
-                num++;
-            }
+            java.util.Arrays.fill(arr, num, num + freq, val);
+            num += freq;
         }
         return arr;
-        /*ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < nums.length - 1; i+=2) {
-            int freq = nums[i];
-            int val = nums[i + 1];
-            for (int j = 1; j <= freq; j++) {
-                list.add(val);
-            }
-        }
-        int[] result = new int[list.size()];
-        int count=0;
-        for (int i:list) {
-            result[count] = i;
-            count++;
-        }
-        return result;*/
     }
 }
