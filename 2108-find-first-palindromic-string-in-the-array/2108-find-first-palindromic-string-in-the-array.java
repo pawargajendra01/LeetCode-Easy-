e0b741eef7ba47fa81136def3c5db36ef732267a
Gajendra Pawar <gajendra101004@gmail.com>
2026-07-18 10:10:42 +0530

    Time: 1 ms (100%), Space: 47.2 MB (96.88%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -5,7 +5,7 @@ class Solution {
         }
         return "";
     }
-    public Boolean palindromeChecker(String str){
+    public boolean palindromeChecker(String str){
         for(int i =0;i<str.length()/2;i++){
             if(str.charAt(i)!=str.charAt(str.length()-1-i)){
                 return false;
