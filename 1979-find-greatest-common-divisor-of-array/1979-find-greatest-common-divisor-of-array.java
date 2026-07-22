4d7032f4f6fd96aae41a0399403c3bcb3745f89a
Gajendra Pawar <gajendra101004@gmail.com>
2026-07-22 09:39:16 +0530

    Time: 0 ms (100%), Space: 45.2 MB (39.9%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.java
@@ -1,16 +1,21 @@
 class Solution {
     public int findGCD(int[] nums) {
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        
-        for (int num : nums) {
-            if (num < min) min = num;
-            if (num > max) max = num;
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+
+        for(int num:nums){
+            if(num>max){
+                max=num;
+            }
+            if(num<min){
+                min=num;
+            }
         }
-                return gcd(min, max);
-    }
-    
-    private int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a % b);
+        while (min != 0) {
+            int temp = min;
+            min = max % min;
+            max = temp;
+        }
+        return max;
     }
 }
